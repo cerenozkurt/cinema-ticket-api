c8be4eedab2659cc1ab03e8fe3b4e4293051e7ee
Ceren Özkurt <cerenozkurt@outlook.com>
2023-02-09 01:58:29 +0300

    payment seed created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Payment;
 use App\Models\Seats;
 use App\Models\Sessions;
 use App\Models\TheatreHall;
@@ -68,7 +69,16 @@ class DatabaseSeeder extends Seeder
             'name' => 'Salon '.$hall
         ]);
         $hall++;
-
        }
+
+       Payment::create([
+        'title' => 'Student',
+        'price' => 30,
+       ]);
+       Payment::create([
+        'title' => 'Adult',
+        'price' => 50,
+       ]);
+      
     }
 }

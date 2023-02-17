a33a5c328c8442e7053e1b2519f83e72de6ba082
Ceren Özkurt <cerenozkurt@outlook.com>
2023-02-17 09:39:41 +0300

    director routes added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CastController;
+use App\Http\Controllers\DirectorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -31,5 +32,17 @@ Route::controller(CastController::class)
     Route::put('/{cast}','update');
     Route::delete('/{cast}','destroy');
     //Route::post('/{cast}/image','upload_image');
+    //Route::delete('/{id}/image','delete_image');
+});
+
+Route::controller(DirectorController::class)
+->prefix('director')
+->group(function(){
+    Route::get('','index');
+    Route::post('','store');
+    Route::get('/{director}','show');
+    Route::put('/{director}','update');
+    Route::delete('/{director}','destroy');
+    //Route::post('/{director}/image','upload_image');
     //Route::delete('/{id}/image','delete_image');
 });
